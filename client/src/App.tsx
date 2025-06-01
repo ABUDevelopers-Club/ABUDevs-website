f2481fb2e9b1d84806ca5009742f668fe723a39e
alimancs <aliman2952003@gmail.com>
2025-06-01 16:30:45 -0700

    client:vite + react + typescript setup and installation of related dependencies

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      Build base
+    </div>
+    
+  )
+}
+
+export default App
---
+++ client/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+// This is a Tailwind CSS configuration file that specifies the content files to scan for class names and extends the default theme.
+// It includes the main HTML file and all JavaScript/TypeScript files in the src directory.
